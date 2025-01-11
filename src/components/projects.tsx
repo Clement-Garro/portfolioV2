af33e2d96de7eaf6eeb5736b6788fe46e2ca78f8
Suyano <101984858+Clement-Garro@users.noreply.github.com>
2025-01-11 10:53:06 +0100

    first push

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,12 +1,12 @@
-import {projectsData} from '@/data/projects'
+import {projectsData} from '@/data/projects';
 import {ThreeDCard} from "@/components/ui/ThreeDCard";
 
 export default function Projects() {
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 flex justify-center">
       <div className="container px-4 md:px-6">
-        {Object.keys(projectsData).map((category: string) => {
-          const projectCount = projectsData[category].length;
+        {Object.keys(projectsData).map((category) => {
+          const projectCount = projectsData[category as keyof typeof projectsData].length;
 
           const gridCols =
             projectCount === 1
@@ -21,7 +21,7 @@ export default function Projects() {
                 {category}
               </h2>
               <div className={`grid ${gridCols} gap-6 justify-center`}>
-                {projectsData[category].map((project: any, index: number) => (
+                {projectsData[category as keyof typeof projectsData].map((project: any, index: number) => (
                   <ThreeDCard
                     key={index}
                     videoSrc={project.videoSrc}
